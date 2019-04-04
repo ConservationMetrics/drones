dabfc5be7af8e565ea820affb2f88df5cf5deccd
Abram B. Fleishman <abram@conservationmetrics.com>
2019-04-04 12:09:55 -0700

    update script

--- Scripts/example_create_tiles.R
+++ Scripts/example_create_tiles.R
@@ -22,7 +22,7 @@ library(drones)
 
 # Tile photos -------------------------------------------------------------
 # figure out how many crops you might want
-img2<-image_read("C:/Users/ConservationMetrics/Desktop/tupu_tagging/Images/P1160482.JPG_3264_0.JPG" )
+img2<-image_read("C:/Users/ConservationMetrics/Downloads/20180825 Island_transparent_mosaic_group1_3_1.tif" )
 img_info<-image_info(img2)
 
 # horizontal option
@@ -37,8 +37,8 @@ n_crop_horz[n_crop_vert%in%n_crop_horz]
 # faster rcnn will downsize to max width = 1024, max height=600 so aim for this
 #  ball park to use the maximum amount of info in an image
 
-n_horz=2
-n_vert=2
+n_horz=5
+n_vert=8
 
 # try the crop
 height=img_info$height/n_vert;
@@ -47,15 +47,15 @@ width=img_info$width/n_horz;
 width;height
 
 crop<-paste0(width,"x",height,"+",width*3,"+",height*2)
-img2<-image_crop(img,geometry =crop,repage = T )
+img2<-image_crop(img2,geometry =crop,repage = T )
 
 #view it (need to be expanded since it will display at full res)
 img2
 
 # Run the function
 
-tile_photos(in_folder='C:/Users/ConservationMetrics/Desktop/tupu_tagging/Images/P1160482.JPG_3264_0.JPG',
-            out_folder=paste0('C:/Users/ConservationMetrics/Desktop/tupu_tagging/Images',"/3tiled"),
-            width = 408, height=306, buff=60,file_type = "JPG")
+tile_photos(in_folder='C:/Users/ConservationMetrics/Downloads/20180825 Island_transparent_mosaic_group1_3_1.tif',
+            out_folder=paste0('C:/Users/ConservationMetrics/Desktop/eseal_temp'),
+            width = width, height=height, buff=0,file_type = "tif")
 
 
